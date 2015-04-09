c6f154004a6aa89e0daedddd1a043f04ec893ce2
krujos <jkruck@pivotal.io>
2015-04-09 15:35:07 -0500

    Add in space reporting

--- report.py
+++ report.py
@@ -12,7 +12,20 @@ for org in orgs['resources']:
     quota = json.loads(check_output(['cf', 'curl', quota_url]))
     quota_memeory_limit = quota['entity']['memory_limit']
     used = json.loads(check_output(['cf', 'curl', '/v2/organizations/' + guid + '/memory_usage']))['memory_usage_in_mb']
-    print "Org " + name + " is using " + str(used) + " of " + str(quota_memeory_limit)
+    print "Org " + name + " is using " + str(used) + "MB of " + str(quota_memeory_limit) + "MB."
+    spaces_url = org['entity']['spaces_url']
+    spaces = json.loads(check_output(['cf', 'curl', spaces_url]))
+    for space in spaces['resources']:
+        apps_url = space['entity']['apps_url']
+        consumed = 0
+        apps = json.loads(check_output(['cf', 'curl', apps_url]))
+        for app in apps['resources']:
+            instances = app['entity']['instances']
+            memory = app['entity']['memory']
+            consumed += (instances * memory)
+        print "\tSpace " + space['entity']['name'] + " is using " + str(consumed) + "MB memory (" + str(100 * consumed / quota_memeory_limit) + "%) of org quota"
+        
 
+                    
 
 
